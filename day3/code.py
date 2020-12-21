0987f765a5a0edbc0616824dc70aff99a72d0312
Stef Boerrigter <stefboerrigter@gmail.com>
2020-12-21 15:29:19 +0100

    Day 3 pt 1

---
+++ day3/code.py
@@ -0,0 +1,44 @@
+#from parse import parse
+import re
+
+lines = list()
+maxCharLines = 0
+numberOfLines = 0
+
+def initializeList():
+    global numberOfLines
+    global maxCharLines
+    f = open("input.txt", "r")
+    for line in f:
+        lines.append(line)
+        numberOfLines = numberOfLines + 1
+        
+    maxCharLines = len(lines[0]) - 1
+    ".....#......#....#........#.#.."
+    #print("Charactes in line: {0} [{1}]".format(len(lines[0]),lines[0]))
+
+def pattern(right, down):
+    global numberOfLines
+    global maxCharLines
+    treesFound = 0
+    print("Following {0:3} right and {1:3} down ({2})".format(right, down, numberOfLines))
+    rCount = 0
+    dCount = 0
+    end = 0
+    while end == 0:
+        rCount = rCount + right
+        dCount = dCount + down
+        if rCount >= maxCharLines:
+            print("Pos = now {0}, max = {1}, becomes {2}".format(rCount, maxCharLines, rCount - maxCharLines))
+            rCount = rCount - maxCharLines
+            
+        if dCount >= numberOfLines:
+            end = 1
+        else:
+            print("Position {0:3} right and {1:3} down: Char = {2}".format(rCount, dCount, lines[dCount][rCount]))
+            if '#' == lines[dCount][rCount]:
+                #print ("Tree")
+                treesFound += 1
+    print("Found {0} trees".format(treesFound))
+initializeList()
+pattern(3, 1)
